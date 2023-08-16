b905b32203deac88e8b80ccb0b3bb3cc20ac0595
foxstudent105410 <ivan.sydorak@yahoo.com>
2023-08-16 11:17:55 +0300

    bot modifying

--- TelegramBot/ConsoleApp1/Program.cs
+++ TelegramBot/ConsoleApp1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Globalization;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Newtonsoft.Json;
+using System.Linq;
 
 class Program
 {
@@ -24,35 +26,61 @@ class Program
         {
             Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
 
-            string[] messageParts = e.Message.Text.Split(' ');
+            if (e.Message.Text == "/get_currencies")
+            {
+                string availableCurrencies = await GetAvailableCurrencies();
+
+                await bot.SendTextMessageAsync(chatId: e.Message.Chat, text: availableCurrencies);
+                return;
+            }
+
+            if (e.Message.Text == "/start")
+            {
+                await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                                   text: "Hello! Please send a message in the format: <currency code> <date>. For example: USD 01.01.2022. The command to see a list of all currencies - /get_currencies");
+                return;
+            }
+
+            string[] messageParts = e.Message.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             if (messageParts.Length == 2)
             {
-                string currencyCode = messageParts[0];
+                string currencyCode = messageParts[0].ToUpper();
                 string date = messageParts[1];
+
+                // validate the date format
+                if (!DateTime.TryParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var parsedDate))
+                {
+                    await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                                   text: "The date format is incorrect. Correct format is dd.MM.yyyy (e.g., 01.01.2020)");
+                    return;
+                }
+
+                // Check if the date is earlier than 4 years from now
+                var fourYearsAgo = DateTime.Now.AddYears(-4);
+                if (parsedDate.CompareTo(fourYearsAgo) < 0)
+                {
+                    await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                                   text: "You can't request a date earlier than 4 years from now.");
+                    return;
+                }
 
                 string exchangeRate = await GetExchangeRate(currencyCode, date);
 
-                if (exchangeRate != null)
+                if (exchangeRate == null)
                 {
-                    await bot.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: $"The exchange rate for {currencyCode} on {date} was {exchangeRate}"
-                    );
+                    await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                                   text: "No data available for this currency. Please enter a valid currency code. For example: USD.");
                 }
-                else
+                else if (exchangeRate != null)
                 {
-                    await bot.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: "No data available for this currency and date. Please enter a valid currency code and date."
-                    );
+                    await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                                   text: $"The exchange rate for {currencyCode} on {date} was {exchangeRate}");
                 }
             }
             else
             {
-                await bot.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text: "Please send a message in the format: <currency code> <date>"
-                );
+                await bot.SendTextMessageAsync(chatId: e.Message.Chat,
+                                               text: "Please send a message in the format: <currency code> <date>");
             }
         }
     }
@@ -66,21 +94,66 @@ class Program
             string responseBody = await response.Content.ReadAsStringAsync();
 
             dynamic data = JsonConvert.DeserializeObject(responseBody);
-            foreach (var rate in data.exchangeRate)
+
+            // Deserializing dynamic object to list of ExchangeRate
+            List<ExchangeRate> exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate>>(JsonConvert.SerializeObject(data.exchangeRate));
+
+            // 2. Validate the currency code
+            var targerCurrency = currencyCode;
+            Console.WriteLine(targerCurrency);
+            List<ExchangeRate> matchingCurrency = exchangeRates.FindAll(currency => currency.Currency == targerCurrency);
+
+            Console.WriteLine(matchingCurrency);
+            foreach (ExchangeRate c in matchingCurrency)
             {
-                if (rate.currency == currencyCode)
-                {
-                    return rate.saleRateNB;
-                }
+                Console.WriteLine(c.SaleRateNB, c.Currency);
+                return c.SaleRateNB;
             }
+
+            return null;
         }
         catch (HttpRequestException)
         {
-            // Handle the exception and return an error message
-            return "An error occurred while retrieving the exchange rate. Check wtitten currency code and date and try again";
+            return "An error occurred while retrieving the exchange rate. Check the written currency code and date, then try again";
         }
-
-        return null;
     }
 
+    private static async Task<string> GetAvailableCurrencies()
+    {
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync("https://api.privatbank.ua/p24api/exchange_rates?date=01.12.2014");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Deserialize JSON response to dynamic object
+            dynamic data = JsonConvert.DeserializeObject(responseBody);
+
+            // Deserialize exchangeRate to List<ExchangeRate>
+            List<ExchangeRate> exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate>>(data.exchangeRate.ToString());
+
+            // Get all available currencies with LINQ
+            var availableCurrencies = exchangeRates.Select(r => r.Currency).Distinct();
+
+            // Construct response message
+            string message = "Available currencies are:";
+            foreach (var currency in availableCurrencies)
+            {
+                message += "\n" + currency;
+            }
+
+            return message;
+        }
+        catch (HttpRequestException)
+        {
+            return "An error occurred while retrieving the available currencies. Please try again later.";
+        }
+    }
+
+
+    public class ExchangeRate
+    {
+        public string Currency { get; set; }
+        public string SaleRateNB { get; set; }
+    }
 }
